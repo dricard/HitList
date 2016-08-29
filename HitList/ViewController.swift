538b5333760c6075d52a75d6dee09bc9dc7f8c65
Denis Ricard <d.ricard@me.com>
2016-08-29 14:11:43 -0400

    Initial Commit

---
+++ HitList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HitList
+//
+//  Created by Denis Ricard on 2016-08-29.
+//  Copyright © 2016 Hexaedre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
